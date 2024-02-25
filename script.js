9790aa1a530e55433f4b906af437fb8fdbd46c98
I k <ilona.koschi@hotmail.com>
2024-02-25 08:58:23 +0100

    implemented convert() function

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const apiURL = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_9Tmj7QonHyhoicdrcYu1fImf4tCCsOKGLlxg5cfQ`;
-
+let exchangeRates = {};
 
 async function init() {
     try {
         const response = await fetch(apiURL);
-        if (!response.ok) {
-            throw new Error('Netzwerkantwort war nicht ok');
-        }
-        const data = await response.json();
-        console.log('Fertig', data);
+        const data = await checkResponse(response);
+        exchangeRates = data.data; 
     } catch (error) {
         console.error('Fehler aufgetreten:', error);
-        errorFunction();
     }
 }
 
-function errorFunction() {
-    console.log('Fehler aufgetreten');
+function checkResponse(response) {
+    if (!response.ok) {
+        throw new Error(`HTTP-Fehler: ${response.status}`);
+    }
+    return response.json();
 }
 
-function convert(){
- 
+function convert() {
+    const fromCurrency = document.getElementById('from').value;
+    const toCurrency = document.getElementById('to').value;
+    const amount = parseFloat(document.getElementById('input').value);
+
+    if (Object.keys(exchangeRates).length === 0) {
+        alert('Wechselkurse nicht geladen. Bitte versuchen Sie es später erneut.');
+        return;
+    }
+
+    const rateFromBase = exchangeRates[fromCurrency];
+    const rateToBase = exchangeRates[toCurrency];
+    const result = amount * (rateToBase / rateFromBase);
+
+    document.getElementById('output').value = result.toFixed(2);
 }
+
+init();
